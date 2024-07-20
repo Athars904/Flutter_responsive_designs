f2588acf59fd42dd52d626d121b523f3dd1f9b9a
athar <athars904@gmail.com>
2024-07-20 13:10:44 +0500

    Elegant-Oak Complete project.

---
+++ elegant-oak/elegant-oak/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:ngamar/app/bindings/home_binding.dart';
+import 'package:ngamar/app/data/constants/constants.dart';
+import 'package:ngamar/app/modules/splash/splash_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(defaultOverlay);
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown],
+  );
+  runApp(const Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      useInheritedMediaQuery: true,
+      builder: (context, child) {
+        return GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            FocusManager.instance.primaryFocus?.unfocus();
+          },
+          child: GetMaterialApp(
+            title: 'Ngamar',
+            debugShowCheckedModeBanner: false,
+            useInheritedMediaQuery: true,
+            locale: DevicePreview.locale(context),
+            builder: DevicePreview.appBuilder,
+            scrollBehavior: const ScrollBehavior().copyWith(overscroll: false),
+            defaultTransition: Transition.fadeIn,
+            theme: AppTheme.lightTheme,
+            initialBinding: HomeBinding(),
+            home: const SplashView(),
+          ),
+        );
+      },
+    );
+  }
+}
